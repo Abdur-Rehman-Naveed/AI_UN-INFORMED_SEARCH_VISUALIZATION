23e345819f57a375883055a9baff47c353ed9b05
Abdur-Rehman-Naveed <abdnav55@gmail.com>
2026-02-17 04:14:12 +0500

    updating visualizer.py by adding bfs
    Added BFS in the main python file

--- visualizer.py
+++ visualizer.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import time
 import numpy as np
 from matplotlib.colors import ListedColormap
-
+from collections import deque
 
 Rows=10
 Cols=10
@@ -53,13 +53,54 @@ def update_screen(grid,start,goal,visited,selected,path):
 
     plt.pause(Delay)
 
+#check validity of the move
+def valid(r, c):
+    return 0 <= r < Rows and 0 <= c < Cols
+
+# to rebuild shortest path
+def reconstruct(parent, goal):
+    path = []
+    while goal in parent:
+        path.append(goal)
+        goal = parent[goal]
+    return path[::-1]
+
+
+#bfs uninformed search algorithm
+
+def bfs(grid, start, goal):
+
+    queue = deque([start])
+    visited = set([start])
+    parent = {}
+    explored = []
+
+    while queue:
+        current = queue.popleft()
+        explored.append(current)
+
+        if current == goal:
+            return reconstruct(parent, goal)
+
+        for move in Directions:
+            new_row, new_col = current[0] + move[0], current[1] + move[1]
+            if valid(new_row, new_col) and (new_row, new_col) not in visited:
+                queue.append((new_row, new_col))
+                visited.add((new_row, new_col))
+                parent[(new_row, new_col)] = current
+
+        update_screen(grid, start, goal, explored, list(queue), [])
+
+    return []
 
 def main():
     grid= np.zeros((Rows,Cols))
     start=(0,0)
     goal=(Rows-1,Cols-1)
     plt.figure(figsize=(6,6))
-    update_screen(grid,start,goal,[],[],[])
+
+    path=bfs(grid,start,goal)
+    update_screen(grid,start,goal,[],[],path)
     plt.show()
 
 if __name__ == "__main__" :
