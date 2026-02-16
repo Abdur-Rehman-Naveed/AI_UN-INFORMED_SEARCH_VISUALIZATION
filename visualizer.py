0532c5ced23c147e54f8c45a2ace90c4e7c63194
Abdur-Rehman-Naveed <abdnav55@gmail.com>
2026-02-17 03:45:21 +0500

    Update visualizer.py
    Displaying the grid and adjusting its appearance

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,7 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import time
 import numpy as np
+from matplotlib.colors import ListedColormap
 
 
 Rows=10
@@ -16,12 +17,50 @@ Directions=[
    (-1, -1)   # Top-Left
 ]
 
+def update_screen(grid,start,goal,visited,selected,path):
+    display=np.copy(grid)
+
+    for r, c in visited:
+        display[r][c] = 2  # visited
+
+    for r, c in selected:
+        display[r][c] = 3  # selected
+
+    for r, c in path:
+        display[r][c] = 4  # path
+
+    display[start] = 5   
+    display[goal] = 6    
+
+    plt.clf()
+    plt.title("UNINFORMED SEARCH VISUALIZER",fontsize=15)
+    colors_map = ListedColormap([
+        "white",   # 0 empty
+        "black",   # 1 (unused)
+        "yellow",  # 2 explored
+        "blue",    # 3 frontier
+        "purple",  # 4 final path
+        "green",   # 5 start
+        "red"      # 6 goal
+    ])
+
+    plt.imshow(display, cmap=colors_map)
+
+    # setting the axis marks and grid lines 
+    plt.xticks(np.arange(-.5, Cols, 1), [])
+    plt.yticks(np.arange(-.5, Rows, 1), [])
+    plt.grid(color='gray', linestyle='-', linewidth=0.5)
+
+    plt.pause(Delay)
 
 
 def main():
     grid= np.zeros((Rows,Cols))
-    print(grid)
-    print("hello")
+    start=(0,0)
+    goal=(Rows-1,Cols-1)
+    plt.figure(figsize=(6,6))
+    update_screen(grid,start,goal,[],[],[])
+    plt.show()
 
 if __name__ == "__main__" :
     main()
